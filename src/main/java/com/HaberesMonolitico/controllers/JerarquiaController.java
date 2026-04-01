08b7cff1db29d94959a32067b06fe196473d2fd0
Ricardo <ricardo_lombardo@hotmail.com>
2026-04-01 10:28:52 +0200

    Servicio de add Jerarquia

--- src/main/java/com/HaberesMonolitico/controllers/JerarquiaController.java
+++ src/main/java/com/HaberesMonolitico/controllers/JerarquiaController.java
@@ -2,12 +2,12 @@ package com.HaberesMonolitico.controllers;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.HaberesMonolitico.entities.Jerarquia;
 import com.HaberesMonolitico.services.JerarquiaService;
 
@@ -27,5 +27,10 @@ public class JerarquiaController {
 	public Optional<Jerarquia> obtenerPorId(Long id){
 		return jerarquiaService.obtenerJerarquiaPorId(id);
 	}
+	
+    @PostMapping
+    public Jerarquia crearJerarquia(@RequestBody Jerarquia jerarquia) {
+        return jerarquiaService.guardarJerarquia(jerarquia);
+    }
 	
 }
